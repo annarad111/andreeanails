991b35d66576a7ef653c46f1dbfb59b9c6fa9abe
Ana <radulescuana111@gmail.com>
2022-03-29 16:55:22 +0300

    Added button links on the gallery page

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,6 +6,13 @@ import nails2 from '../images/nails2.jpg';
 import nails3 from '../images/nails3.jpg';
 import nails4 from '../images/nails4.jpg';
 import nails5 from '../images/nails5.jpg';
+import nails6 from '../images/nails8.jpg';
+import nails7 from '../images/nails7.jpg';
+import nails8 from '../images/nails9.jpg';
+import nails9 from '../images/nails10.jpg';
+import nails10 from '../images/nails11.jpg';
+import nails11 from '../images/nails12.jpg';
+import nails12 from '../images/nails13.jpg';
 import '../components/Carousel.css'
 
 
@@ -15,6 +22,13 @@ const items = [
     <div className="item" data-value="3"><img src={nails3} className="itemimg"></img></div>,
     <div className="item" data-value="4"><img src={nails4} className="itemimg"></img></div>,
     <div className="item" data-value="5"><img src={nails5} className="itemimg"></img></div>,
+    <div className="item" data-value="6"><img src={nails6} className="itemimg"></img></div>,
+    <div className="item" data-value="7"><img src={nails7} className="itemimg"></img></div>,
+    <div className="item" data-value="8"><img src={nails8} className="itemimg"></img></div>,
+    <div className="item" data-value="9"><img src={nails9} className="itemimg"></img></div>,
+    <div className="item" data-value="10"><img src={nails10} className="itemimg"></img></div>,
+    <div className="item" data-value="11"><img src={nails11} className="itemimg"></img></div>,
+    <div className="item" data-value="12"><img src={nails12} className="itemimg"></img></div>,
 ];
 
 const Carousel = () => (
--- src/pages/Galerie.jsx
+++ src/pages/Galerie.jsx
@@ -1,11 +1,19 @@
 import Carousel from '../components/Carousel';
 import nails1 from '../images/nails1.jpg';
+import '../pages/Galerie.css';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 export default function Galerie(){
     return(
         <>
-        <h1>Galerie</h1>
+        <h1 className='titlugalerie'>Galerie</h1>
         <Carousel></Carousel>
+        <div className='galerielinks'>
+            <a href="https://www.facebook.com/AndreeaNailsNr1/?ref=page_internal" target="_blank" className='galeriea'><FavoriteBorderIcon/>Facebook</a>
+            <a href="https://wa.me/+40762727555" target="_blank" className='galeriea'><FavoriteBorderIcon/>WhatsApp</a>
+            <a href="tel:+40762727555" target="_blank" className='galeriea'><FavoriteBorderIcon/>Telefon</a>
+
+        </div>
         
         </>
     )
