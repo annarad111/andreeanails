68ded2e676b110db0b0dc599d550ad78b882ae81
Ana <radulescuana111@gmail.com>
2022-03-29 14:48:19 +0300

    Added to Contact page google maps

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,20 @@
+import React from "react";
+import "./Contact.css";
+import Map from '../components/Map';
+
+
 export default function Contact(){
+    const location = {
+        address: 'Strada Marasesti 61, Orasti, Frumusani',
+        lat: 44.3205551,
+        lng: 26.2902006,
+      }
+
+
     return(
-    <h1>Hello</h1>
+        <>
+    <h1>Contact</h1>
+    <Map location={location} zoomLevel={19} />
+    </>
     )
 }
