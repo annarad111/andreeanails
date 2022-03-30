a653d814145689b529b69a55549ff5780593312f
Ana <radulescuana111@gmail.com>
2022-03-30 11:37:41 +0300

    Added links to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Contact.css";
 import Map from '../components/Map';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 
 export default function Contact(){
@@ -15,6 +16,11 @@ export default function Contact(){
         <>
     <h1>Contact</h1>
     <Map location={location} zoomLevel={19} />
+    <div className="contactdetails">
+        <FavoriteIcon></FavoriteIcon><a href="tel:+40762727555" className="contactlinks" target="_blank">+4076 27 27 555</a>
+        <FavoriteIcon></FavoriteIcon><a href="https://wa.me/+40762727555" className="contactlinks" target="_blank">Programari</a>
+        <FavoriteIcon></FavoriteIcon><a href="https://www.facebook.com/AndreeaNailsNr1/?ref=page_internal" className="contactlinks" target="_blank">Facebook</a>
+        </div>
     </>
     )
 }
